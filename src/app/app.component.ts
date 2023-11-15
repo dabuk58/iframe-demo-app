b2f83850513ba9b413984dc62fde7cac1bec80be
dabuk58 <jakub.deska@simpleformat.pl>
2023-11-15 11:52:20 +0100

    sending password an dlogin opnly after iframe init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'iframe-demo-app';
+export class AppComponent implements OnInit {
+  formGroup!: FormGroup;
+  isLoggedIn: boolean = false;
+
+  constructor(){ }
+
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      login: new FormControl(null, {
+        updateOn: 'change',
+        validators: Validators.required
+      }),
+      password: new FormControl(null, {
+        updateOn: 'change',
+        validators: Validators.required
+      })
+    });
+
+    window.parent.postMessage(true, "http://localhost:4200");
+
+    window.addEventListener("message", (event) => {
+      if(event.origin !== "http://localhost:4200"){
+        return;
+      } else {
+        this.formGroup.patchValue({
+          login: event.data.login,
+          password: event.data.password
+        });
+      }
+    });
+  }
+
+  onLogin(){
+    this.isLoggedIn = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatListModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
